df490479ac57dcb7ccce0ac4331b96af26a34c5e
osmancikjan <osmancikjan@osmancikjans-MacBook-Air.local>
2017-10-23 18:29:04 +0200

    Initial Commit

---
+++ ADR RegistrTests/ADR_RegistrTests.swift
@@ -0,0 +1,36 @@
+//
+//  ADR_RegistrTests.swift
+//  ADR RegistrTests
+//
+//  Created by osmancikjan on 10/23/17.
+//  Copyright © 2017 Jan Osmancik. All rights reserved.
+//
+
+import XCTest
+@testable import ADR_Registr
+
+class ADR_RegistrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
