0db0bbeca9cdba8b3eec33ad17bf6be0ab3b93d7
Jan Osmančík <j.osmancik@gmail.com>
2017-10-26 22:35:42 +0200

    Un, Kemler a Nazev hledani funkcni

--- ADR Registr/ViewController.swift
+++ ADR Registr/ViewController.swift
@@ -8,18 +8,151 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var txtKemler: UITextField!
+    @IBOutlet weak var txtUn: UITextField!
+    
+    var parsedData = [Latka]()
+    var filteredData = [Latka]()
+    var isNazevSearching = false
+    var isUnSearching = false
+    var isKemlerSearching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ParseMinimalData()
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+        txtUn.delegate = self
+        txtUn.addTarget(self, action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)), for: UIControlEvents.editingChanged)
+        txtKemler.delegate = self
+        txtKemler.addTarget(self, action: #selector(UITextFieldDelegate.textFieldShouldEndEditing(_:)), for: UIControlEvents.editingChanged)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isNazevSearching || isUnSearching || isKemlerSearching {
+            return filteredData.count
+        }
+        return parsedData.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        if isNazevSearching || isUnSearching || isKemlerSearching {
+            cell?.textLabel?.text = filteredData[indexPath.row].nazev
+            cell?.detailTextLabel?.text = "UN: " + filteredData[indexPath.row].un + ", KEMLER: " + filteredData[indexPath.row].kemler
+        }
+        else {
+            cell?.textLabel?.text = parsedData[indexPath.row].nazev
+            cell?.detailTextLabel?.text = "UN: " + parsedData[indexPath.row].un + ", KEMLER: " + parsedData[indexPath.row].kemler
+        }
+        return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        if searchBar.text == nil || searchBar.text == "" {
+            isNazevSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isNazevSearching = true
+            filteredData = parsedData.filter{
+                $0.nazevBezDiakritiky.lowercased().contains(searchBar.text!.lowercased()) || $0.nazevBezDiakritiky.lowercased().contains(searchBar.text!.lowercased())
+            }
+            tableView.reloadData()
+        }
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.isEqual(txtUn) {
+            txtKemler.text = nil
+            if (txtUn.text?.characters.count)! > 4 {
+                txtUn.deleteBackward()
+            }
+            if (textField.text?.isEmpty)! {
+                isUnSearching = false
+                view.endEditing(true)
+                tableView.reloadData()
+            } else {
+                isUnSearching = true
+                print(textField.text!)
+                filteredData = parsedData.filter{
+                    $0.un.contains(textField.text!)
+                }
+                tableView.reloadData()
+            }
+        }
+        if textField.isEqual(txtKemler) {
+            txtUn.text = nil
+            if (txtKemler.text?.characters.count)! > 4 {
+                txtKemler.deleteBackward()
+            }
+            if (textField.text?.isEmpty)! {
+                isKemlerSearching = false
+                view.endEditing(true)
+                tableView.reloadData()
+            } else {
+                isKemlerSearching = true
+                print(textField.text!)
+                filteredData = parsedData.filter{
+                    $0.kemler.contains(textField.text!.uppercased())
+                }
+                tableView.reloadData()
+            }
+        }
+        return true
+    }
+    
+    func ParseMinimalData (){
+        guard let path = Bundle.main.path(forResource: "registr", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            guard let array = json as? [Any] else { return }
+            
+            for item in array {
+                guard let dict = item as? [String : Any] else { return }
+                guard let kemler = dict["KEMLER"] as? String else { return }
+                guard let nazev = dict["LATKA"] as? String else { return }
+                guard let nazevBezDiakritiky = dict["LATKABEZDIAKRITIKY"] as? String else { return }
+                guard let un = dict["UN"] as? String else { return }
+                parsedData.append(Latka(un : un, nazev : nazev, nazevBezDiakritiky : nazevBezDiakritiky, kemler : kemler))
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
 
-
+class Latka {
+    var un : String!
+    var nazev : String!
+    var nazevBezDiakritiky : String!
+    var kemler : String!
+    
+    init(un : String, nazev : String, nazevBezDiakritiky: String, kemler : String) {
+        self.un = un
+        self.nazev = nazev
+        self.nazevBezDiakritiky = nazevBezDiakritiky
+        self.kemler = kemler
+    }
 }
 
